2c1bb4c815f5d01c82fc3ffa664a445d7f09225a
neotag <harada@gaji.jp>
2020-06-19 16:12:15 +0900

    Add Home story

---
+++ pages/index.story.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { linkTo } from "@storybook/addon-links";
+import Home from "./index";
+
+export default { title: "Welcome" };
+
+export const toNext = () => <Home />;
