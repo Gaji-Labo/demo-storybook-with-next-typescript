f36de532355c827b537c66053cdf163f131f19a5
neotag <harada@gaji.jp>
2020-08-26 14:46:38 +0900

    Use Material-UI

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,30 @@
-const addParameters = require("@storybook/react").addParameters;
+import { addParameters, addDecorator } from "@storybook/react";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {story()}
+  </ThemeProvider>
+));
 
 addParameters({
   options: {
